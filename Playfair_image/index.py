f5b18fa0a698f3f4a2b33329a00bccff25aa2e3f
bgmi17 <121046777+bgmi17@users.noreply.github.com>
2023-11-06 10:36:18 +0530

    Add files via upload

--- Playfair_image/index.py
+++ Playfair_image/index.py
@@ -1,50 +1,28 @@
-import string
 import numpy as np
 from PIL import Image
+from functools import reduce
 
-def prepare_key(key):
-    # Remove whitespaces and convert to uppercase
-    key = key.replace(" ", "").upper()
+def unique(list1):
+    ans = reduce(lambda re, x: re+[x] if x not in re else re, list1, [])
+    return ans
 
-    # Remove duplicate characters while maintaining the order
-    key = "".join(dict.fromkeys(key))
-
-    # Add remaining characters (letters, numbers, and special characters) to the key
-    remaining_chars = [chr(ch) for ch in np.arange(255,-1,-1).tolist() if chr(ch) not in key]
-    key += "".join(remaining_chars)
-
+def create_key(key):
+    key = unique(key)
+    for i in np.arange(255,-1,-1).tolist():
+        if i not in key:
+            key.append(i)
+    key = np.asarray(key).reshape((16,16))
     return key
 
-def create_playfair_matrix(key):
-    key = prepare_key(key)
-    matrix = [['' for _ in range(16)] for _ in range(16)]
-    char_index = 0
-
+def find_char_position(matrix, char):
     for row in range(16):
         for col in range(16):
-            if char_index < len(key):
-                matrix[row][col] = key[char_index]
-                char_index += 1
-
-    return matrix
-
-
-def prepare_message(message):
-    message = message.upper()
-    message = message.replace("J", "I")  # Replace 'J' with 'I'
-    message = "".join(filter(str.isalnum, message))  # Remove non-alphanumeric characters
-
-    # Make the message length even by adding an 'X' at the end if necessary
-    if len(message) % 2 != 0:
-        message += 'X'
-
-    return message
+            if matrix[row][col] == char:
+                return row, col
 
 def encrypt_playfair(message, key):
-    matrix = create_playfair_matrix(key)
-    message = prepare_message(message)
-
-    encrypted_message = ""
+    matrix = create_key(key)
+    encrypted_message = []
     i = 0
 
     while i < len(message):
@@ -52,26 +30,56 @@ def encrypt_playfair(message, key):
         i += 1
         char2 = message[i]
         i += 1
-
         row1, col1 = find_char_position(matrix, char1)
         row2, col2 = find_char_position(matrix, char2)
 
         if row1 == row2:
-            encrypted_message += matrix[row1][(col1 + 1) % 8]
-            encrypted_message += matrix[row2][(col2 + 1) % 8]
+            encrypted_message.append(matrix[row1][(col1 + 1) % 16])
+            encrypted_message.append(matrix[row2][(col2 + 1) % 16])
         elif col1 == col2:
-            encrypted_message += matrix[(row1 + 1) % 8][col1]
-            encrypted_message += matrix[(row2 + 1) % 8][col2]
+            encrypted_message.append(matrix[(row1 + 1) % 16][col1])
+            encrypted_message.append(matrix[(row2 + 1) % 16][col2])
         else:
-            encrypted_message += matrix[row1][col2]
-            encrypted_message += matrix[row2][col1]
+            encrypted_message.append(matrix[row1][col2])
+            encrypted_message.append(matrix[row2][col1])
 
     return encrypted_message
 
-def decrypt_playfair(encrypted_message, key):
-    matrix = create_playfair_matrix(key)
+import random
+key = [random.randint(0,255) for i in range(255)]
+print(key)
 
-    decrypted_message = ""
+image = Image.open('/content/download.jpg')
+image = image.resize((224,224))
+image = np.asarray(image)
+
+
+
+
+
+
+
+
+
+
+
+
+Image.fromarray(image)
+def channelSplit(image):
+    return np.dsplit(image,image.shape[-1])
+
+[B,G,R]=channelSplit(image)
+b = np.asarray(encrypt_playfair(B.reshape((-1)).tolist(),key)).reshape((224,224))
+g = np.asarray(encrypt_playfair(G.reshape((-1)).tolist(),key)).reshape((224,224))
+r = np.asarray(encrypt_playfair(R.reshape((-1)).tolist(),key)).reshape((224,224))
+
+image = np.array(np.dstack((b,g,r)),dtype=np.uint8)
+Image.fromarray(image)
+
+def decrypt_playfair(encrypted_message, key):
+    matrix = create_key(key)
+
+    decrypted_message = []
     i = 0
 
     while i < len(encrypted_message):
@@ -84,28 +92,22 @@ def decrypt_playfair(encrypted_message, key):
         row2, col2 = find_char_position(matrix, char2)
 
         if row1 == row2:
-            decrypted_message += matrix[row1][(col1 - 1) % 8]
-            decrypted_message += matrix[row2][(col2 - 1) % 8]
+            decrypted_message.append(matrix[row1][(col1 - 1) % 16])
+            decrypted_message.append(matrix[row2][(col2 - 1) % 16])
         elif col1 == col2:
-            decrypted_message += matrix[(row1 - 1) % 8][col1]
-            decrypted_message += matrix[(row2 - 1) % 8][col2]
+            decrypted_message.append(matrix[(row1 - 1) % 16][col1])
+            decrypted_message.append(matrix[(row2 - 1) % 16][col2])
         else:
-            decrypted_message += matrix[row1][col2]
-            decrypted_message += matrix[row2][col1]
+            decrypted_message.append(matrix[row1][col2])
+            decrypted_message.append(matrix[row2][col1])
 
     return decrypted_message
 
-def find_char_position(matrix, char):
-    for row in range(8):
-        for col in range(8):
-            if matrix[row][col] == char:
-                return row, col
-
-image = Image.open('download.jpg')
-np.asarray(image).shape
-
-def channelSplit(image):
-    return np.dsplit(image,image.shape[-1])
-
 [B,G,R]=channelSplit(image)
-G
+print(B.shape)
+b = np.asarray(decrypt_playfair(B.reshape((-1)).tolist(),key)).reshape((224,224))
+g = np.asarray(decrypt_playfair(G.reshape((-1)).tolist(),key)).reshape((224,224))
+r = np.asarray(decrypt_playfair(R.reshape((-1)).tolist(),key)).reshape((224,224))
+
+image = np.array(np.dstack((b,g,r)),dtype=np.uint8)
+Image.fromarray(image)
